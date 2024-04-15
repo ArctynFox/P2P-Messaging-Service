96a728d8a737c719900507cb255788641e6507d7
Andrea Florence <andrea.florence180@topper.wku.edu>
2024-04-15 13:09:35 -0500

    UI Implementation
    Created folder for holding various UI components to utilize; added implementation to client side.

--- server/src/ClientConnectionThread.java
+++ server/src/ClientConnectionThread.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.net.Socket;
@@ -11,10 +12,43 @@ public class ClientConnectionThread extends Thread {
     DataOutputStream out;
     BufferedReader in;
 
+    // Swing components for UI
+    JFrame frame;
+    JTextArea chatArea;
+    JTextField messageField;
+    JButton sendButton;
+
     ClientConnectionThread(Socket socket, Connection db, int id) {
         this.socket = socket;
         this.db = db;
         this.threadID = id;
+
+        // Initialize UI components
+        frame = new JFrame("Chat App");
+        chatArea = new JTextArea();
+        messageField = new JTextField();
+        sendButton = new JButton("Send");
+
+        // Set layout
+        frame.setLayout(new BorderLayout());
+        frame.add(chatArea, BorderLayout.CENTER);
+        frame.add(messageField, BorderLayout.SOUTH);
+        frame.add(sendButton, BorderLayout.EAST);
+
+        // Set frame properties
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        // Add action listener to send button
+        sendButton.addActionListener(e -> sendMessage());
+    }
+
+    // Method to send message
+    private void sendMessage() {
+        String message = messageField.getText();
+        // Send message to server
+        // You need to implement this part
     }
 
     @Override
@@ -25,3 +59,4 @@ public class ClientConnectionThread extends Thread {
         //such as getting a user hash, updating their own user IP entry, or obtaining the IP of a given user hash
     }
 }
+
